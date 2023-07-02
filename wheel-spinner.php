78e03b260fd552063523c04bec2224506c0d197c
DCerm <138310097+DCerm@users.noreply.github.com>
2023-07-02 15:58:42 +0100

    Update wheel-spinner.php

--- wheel-spinner.php
+++ wheel-spinner.php
@@ -1,6 +1,28 @@
 <?php
 /**
- * Plugin Name: WordPress Vue Tutorial
- * Description: A demo on how to use Vue in WordPress.
+ * Plugin Name: Wheel-spinner
+ * Description: A prize wheel plugin.
  */
-?>
+
+//Register Scripts to use 
+function func_load_vuescripts() {
+ wp_register_script( 'wpvue_vuejs', 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js');
+}
+add_action('wp_enqueue_scripts', 'func_load_vuescripts');
+//change to vue.min.js for production
+
+//Wheel spinner function
+ function func_wheel_spinner(){
+  //Add Vue.js
+  wp_enqueue_script('wpvue_vuejs');
+
+   //Build String
+   $str= "<div id='official-wheel'>"
+    ."Message from Vue: "
+    ."</div>";
+  return $str;
+} // end function
+
+//Add shortscode
+add_shortcode( 'officialcorvusaurumwheel', 'func_wheel_spinner' );
+//End of shortcode
